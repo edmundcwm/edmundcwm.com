8d3051a2f281ada008d5dbb96763d38c1c36f54b
Edmund Chan <edmundcwm@gmail.com>
2021-05-30 23:44:17 +0800

    Render Header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Header from '../components/header';
 
 export default function Home() {
-  return (
-   <h1 className="text-lg text-red-400">Welcome to my blog</h1>
-  )
+	return (
+		<>
+			<Header />
+		</>
+	);
 }
