1b0e4e39c1e538ee13e0bd3f404d60b0882ec690
Edmund Chan <edmundcwm@gmail.com>
2021-05-30 23:43:10 +0800

    Extend colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				primary: '#2980b9',
+				body: '#393e46'
+			},
+			fontFamily: {
+				ubuntu: ['Ubuntu']
+			}
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
